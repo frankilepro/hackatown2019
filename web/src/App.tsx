1dbf4bd441c6bfed3073a78c427d51d6fd198af7
Alexis Loiselle <alexisloiselle97@hotmail.com>
2019-01-19 23:25:55 -0500

    fullpage-scroll

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,37 @@
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
+// tslint:disable-next-line
+const ReactFullpage = require("@fullpage/react-fullpage").default;
 import * as React from 'react';
 import './App.css';
-
 import Cards from './components/Cards';
 import CustomMap from './components/CustomMap';
+import Landing from './components/Landing';
 
-@observer
 class App extends React.Component {
-  @observable public data = [];
 
-  public render() {
-    return (
-      <div className="App">
-        <CustomMap />
-        <Cards />
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <ReactFullpage
+                    debug={true} /* Debug logging */
+                    scrollOverflow={true}
+                    navigation={true}
+                    // tslint:disable-next-line:jsx-no-lambda
+                    render={() => (
+                        <ReactFullpage.Wrapper>
+                            <Landing />
+                            <div className="section">
+                                <CustomMap />
+                                <Cards />
+                            </div>
+                            <div className="section">
+                                <h1>text2</h1>
+                            </div>
+                        </ReactFullpage.Wrapper>
+                    )}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
